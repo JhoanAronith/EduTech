88cdfffbe289a81bb48338e5f9b8c67ca5a99364
Jhoan <jhoanmunozman@gmail.com>
2024-10-14 10:11:34 -0500

    Primer Avance del Proyecto

---
+++ src/main/java/com/Edutech/Controlador_Pagina.java
@@ -0,0 +1,57 @@
+package com.Edutech;import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador_Pagina {
+    @GetMapping("/inicio-edutech")
+    public String Inicio(Model modelo) {
+        return "inicio";
+    }
+    
+    @GetMapping("/cursos-edutech")
+    public String Cursos(Model modelo) {
+        return "cursos";
+    }
+    
+    @GetMapping("/login-edutech")
+    public String Login(Model modelo) {
+        return "login";
+    }
+    
+    @GetMapping("/registro-edutech")
+    public String Registro(Model modelo) {
+        return ("registro");
+    }
+    
+    @GetMapping("/perfil-edutech")
+    public String Perfil(Model modelo) {
+        return ("perfil");
+    }
+    
+    @GetMapping("/pago-edutech")
+    public String Pago(Model modelo) {
+        return ("pago");
+    }
+    
+    @GetMapping("/nosotros-edutech")
+    public String Nosotros(Model modelo) {
+        return ("nosotros");
+    }
+    
+    @GetMapping("/curso-edutech")
+    public String Curso(Model modelo){
+        return ("curso");
+    }
+    
+    @GetMapping("/certificaciones-edutech")
+    public String Certificaciones(Model modelo) {
+        return ("certificaciones");
+    }
+    
+    @GetMapping("/categorias-edutech")
+    public String Categorias(Model modelo) {
+        return ("categorias");
+    }
+    
+}
