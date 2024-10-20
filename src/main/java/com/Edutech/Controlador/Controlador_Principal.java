61f563d69610cebc9ba00a37e3bbe54117aec1de
Jhoan <jhoanmunozman@gmail.com>
2024-10-20 11:28:59 -0500

    Creación de nuevos paquetes

--- src/main/java/com/Edutech/Controlador/Controlador_Principal.java
+++ src/main/java/com/Edutech/Controlador/Controlador_Principal.java
@@ -1,9 +1,11 @@
-package com.Edutech;import org.springframework.stereotype.Controller;
+package com.Edutech.Controlador;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controlador_Pagina {
+public class Controlador_Principal {
     @GetMapping("/inicio-edutech")
     public String Inicio(Model modelo) {
         return "inicio";
